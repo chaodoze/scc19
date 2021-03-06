319e863cc4b7dec633a461ce8dd3c28705e4895b
chao <chao@nextsmallthings.com>
2021-03-05 16:26:55 -0800

    writeDb() script

--- cities.py
+++ cities.py
@@ -1,6 +1,7 @@
 import csv
 import git
 import json
+import sqlite_utils
 
 def iterate_file_versions(repo_path, filepath, ref='master'):
   repo = git.Repo(repo_path, odbt=git.GitDB)
@@ -24,3 +25,20 @@ def write_csv():
         filtered_city['date'] = when
         writer.writerow(filtered_city)
 
+def write_db(db_name='./scc.db'):
+  city_data = ['geo_id', 'city', 'population', 'cases']
+  db = sqlite_utils.Database(db_name)
+  it = iterate_file_versions(".", "cases_by_city.json", 'master')
+  print(db.table_names())
+  db["city_cases"].delete_where()
+  for (when, hash, data) in it:
+    cities = json.loads(data)
+    filtered_cities = [{k: city[k] for k in city_data} for city in cities]
+    for city in filtered_cities:
+      city['date'] = when
+    db['city_cases'].insert_all(filtered_cities)
+    # for city in cities:
+    #   db["city_cases"].
+    #   do nothing
+if __name__ == "__main__":
+  write_db()
